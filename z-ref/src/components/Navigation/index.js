81f8f35c6f0c69eca206b915139b0db67dd962b0
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-05 13:12:09 -0700

    cleanup

--- z-ref/src/components/Navigation/index.js
+++
@@ -1,8 +0,0 @@
-import MainNav from './MainNav'
-// mobile specifc nav
-
-
-// ./src/components/Navigation -> { key:value, MainNav: <MainNav> }
-export { 
-    MainNav
-}
--- z-ref/src/pages/Error/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Error({error}){
-    return(
-    <section>
-    <h2>404 - Page Not Found</h2>
-    <p>{error ? error.message : null }</p>
-    </section>
-    )
-}
