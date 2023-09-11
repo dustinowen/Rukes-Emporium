753cd1bc33e54912ae77aa59f85e91101f837ef9
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-09-11 08:39:37 -0700

    Finalize CSS for product pages

--- src/pages/item-category/Threads/Threads.jsx
+++ src/pages/item-category/Threads/Threads.jsx
@@ -31,7 +31,7 @@ export default function Threads(inventoryItem) {
                 ${prodCost}
               </p>
 
-              <button className=" mx-36 my-3 relative flex items-center justify-center text-center rounded-md border border-transparent bg-gray-400 px-6 py-2 text-sm font-medium text-gray-900 hover:bg-black hover:text-white"onClick={() => handleClickAdd(inventoryItem)}>
+              <button className=" mx-40 my-3 relative flex items-center justify-center text-center rounded-md border border-transparent bg-gray-400 px-6 py-2 text-sm font-medium text-gray-900 hover:bg-black hover:text-white"onClick={() => handleClickAdd(inventoryItem)}>
                   add to cart
                 </button>
             </div>
